c89451a8936ad98478de181b0fdc1ecce851fdbd
zyellowhorse <zacharyyellowhorse@gmail.com>
2022-01-22 04:08:18 -0700

    Added prompt for text to append and Now having it save the file with the same file name

--- scripts/append-text.sh
+++ scripts/append-text.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
-cat $ENV_INPUTFILE >> outputfile.txt
+fileName=${1}
+
+cat $ENV_INPUTFILE > $fileName
+cat $ENV_TEXTINPUT >> $fileName
 
 echo "Successfully uploaded file and appended text to file. You can find your file in the storage directory."
